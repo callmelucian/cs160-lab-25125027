f17574322a85b19ed5aeb710600ddcf156e39cbf
callmelucian <luciannguyen07@gmail.com>
2025-12-20 16:19:54 +0700

    Refine folder structure

--- utility/geometry.hpp
+++ utility/geometry.hpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 // CONSTANTS
-const double eps = 1e-6; // an arbritary small number
-const double oo = 1e9; // an arbritary large number
+const double eps = 1e-6;
+const double oo = 1e9;
 
 // HELPER FUNCTIONS
 double squared (double a) { return a * a; }
@@ -17,6 +17,8 @@ double squared (double a) { return a * a; }
 // POINT STRUCT TO REPRESENT 2D POINTS AND VECTORS
 struct Point {
     double x, y;
+
+    // Constructors
     Point() : x(0), y(0) {}
     Point (double x, double y) : x(x), y(y) {}
 
@@ -54,10 +56,12 @@ struct Segment {
     double A, B, C; // Ax + by + C = 0
     Point a, b;
 
+    // Constructors
     Segment() : A(0), B(0), C(0) {}
     Segment (Point a, Point b) :
         A(a.y - b.y), B(b.x - a.x), C(cross(a, b)), a(a), b(b) {}
 
+    // Find the euclide distance between a Segment and a Point
     friend double euclideDist (const Segment &d, const Point &p) {
         if (dot(d.b - d.a, p - d.b) <= 0 && dot(d.a - d.b, p - d.a) <= 0) { // projection is on the line
             double numer = abs(d.A * p.x + d.B * p.y + d.C);
@@ -67,6 +71,7 @@ struct Segment {
         else return min(euclideDist(p, d.a), euclideDist(p, d.b));
     }
 
+    // Find the project between a Semgent and a Point
     friend Point projection (const Segment &d, const Point &p) {
         if (dot(d.b - d.a, p - d.b) <= 0 && dot(d.a - d.b, p - d.a) <= 0) { // projection is on the line
             double numer = d.A * p.x + d.B * p.y + d.C;
@@ -85,7 +90,8 @@ private:
     double totalLength;
 
 public:
-    // Constructor
+    // Constructors
+    Polyline() : totalLength(0.0) {}
     Polyline (const vector<Point> &init) : container(init), totalLength(0.0) {
         for (int i = 1; i < container.size(); i++)
             totalLength += euclideDist(container[i - 1], container[i]);
@@ -125,5 +131,40 @@ public:
         if (size())
             totalLength -= euclideDist(container.back(), toRemove);
         return toRemove;
+    }
+    void clear() { container.clear(), totalLength = 0.0; }
+
+    // Extract a portion of the polyline using the projection of a Point to itself
+    Polyline extract (const Point &p, bool toEnd) {
+        if (!size()) return Polyline();
+
+        // find closest segemet
+        double close = oo; Segment closeSegment; int closePoint = 0;
+        for (int i = 1; i < size(); i++) {
+            Segment s(container[i - 1], container[i]);
+            double current = euclideDist(s, p);
+            if (current < close)
+                close = current, closeSegment = s, closePoint = i;
+        }
+        Polyline ans;
+
+        // extract from projection to endpoint
+        if (toEnd) {
+            ans.append(projection(closeSegment, p));
+            for (int i = closePoint; i < size(); i++) ans.append(container[i]);
+        }
+        // extract from startpoint to projection
+        else {
+            for (int i = 0; i < closePoint; i++) ans.append(container[i]);
+            ans.append(projection(closeSegment, p));
+        }
+        return ans;
+    }
+
+    // Output operator
+    friend ostream& operator<< (ostream &ouf, const Polyline &pll) {
+        ouf << "<Polyline of length " << pll.length() << ": ";
+        for (int i = 0; i < pll.size(); i++) cout << pll[i] << " ";
+        ouf << "/>";
     }
 };
